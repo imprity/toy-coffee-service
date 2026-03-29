91823c7187b4a9e6528f5688e7ebbc5e58ae7239
imprity <imprity041@gmail.com>
2026-03-30 02:15:06 +0900

    REFACTOR: GetCoffeeResponse에 생성시간, 수정 시간 추가

--- src/main/java/coffee/server/domain/coffee/dto/GetCoffeeResponse.java
+++ src/main/java/coffee/server/domain/coffee/dto/GetCoffeeResponse.java
@@ -3,15 +3,24 @@ package coffee.server.domain.coffee.dto;
 import coffee.server.domain.coffee.entity.Coffee;
 import coffee.server.domain.coffee.enums.CoffeeStatus;
 import java.math.BigDecimal;
+import java.time.Instant;
 
 public record GetCoffeeResponse(
-        Long coffeeId, String coffeeName, BigDecimal coffeePrice, Long coffeeStock, CoffeeStatus coffeeStatus) {
+        Long coffeeId,
+        String coffeeName,
+        BigDecimal coffeePrice,
+        Long coffeeStock,
+        CoffeeStatus coffeeStatus,
+        Instant createdAt,
+        Instant modifiedAt) {
     public static GetCoffeeResponse of(Coffee coffee) {
         return new GetCoffeeResponse(
                 coffee.getCoffeeId(),
                 coffee.getCoffeeName(),
                 coffee.getCoffeePrice(),
                 coffee.getCoffeeStock(),
-                coffee.getCoffeeStatus());
+                coffee.getCoffeeStatus(),
+                coffee.getCreatedAt(),
+                coffee.getModifiedAt());
     }
 }
