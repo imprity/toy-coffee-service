f1f79037b432a5116fe2b370d1a766d56b3e4b64
imprity <imprity041@gmail.com>
2026-04-01 14:18:53 +0900

    FEAt: 초기 coffee와 point는 설정이 있을때만 넣기
    이제
    
    app.add-test-coffees.enabled
    
    있을때만 초기 coffee를 추가하고
    
    app.add-init-point.enabled
    
    있을때만 초기 point를 추가합니다.

--- src/main/java/coffee/server/common/initializer/CoffeeInitializer.java
+++ src/main/java/coffee/server/common/initializer/CoffeeInitializer.java
@@ -8,11 +8,13 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "app.add-test-coffees.enabled", havingValue = "true", matchIfMissing = false)
 public class CoffeeInitializer implements ApplicationRunner {
     private final CoffeeRepository coffeeRepository;
 
--- src/main/java/coffee/server/common/initializer/PointInitializer.java
+++ src/main/java/coffee/server/common/initializer/PointInitializer.java
@@ -7,11 +7,13 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "app.add-init-point.enabled", havingValue = "true", matchIfMissing = false)
 public class PointInitializer implements ApplicationRunner {
     private final PointRepository pointRepository;
 
